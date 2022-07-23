ef86de0b8885cb646085fe6b62c5ed65731c0593
fms2014 <fms2014@naver.com>
2022-07-23 11:51:48 +0900

    테스트 뷰 생성

---
+++ src/views/ListTest.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="list-test">
+    <list v-on:clickEvent="test" :kind="'directMsg'">
+      <list-item :kind="'directMsg'" :listItem="listItem" v-for="listItem in listItems" :key="listItem.id" ></list-item>
+    </list>
+  </div>
+</template>
+
+<script>
+import List from '@/components/List.vue'
+import ListItem from '@/components/ListItem.vue'
+
+export default {
+  name: 'listTest',
+  components: { List, ListItem },
+  data(){
+    return{
+      listItems:[
+        'test1',
+        'test2',
+        'test3'
+      ]
+    }
+  },
+  methods:{
+    test() {
+      console.log("추가");
+      let len = this.$data.listItems.length + 1;
+      this.$data.listItems.push("test" + len);
+    }
+  }
+}
+</script>
