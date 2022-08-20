7e636d04bfdf2d4edb6084c147f66fcfeb5430fb
fms2014 <fms2014@naver.com>
2022-08-20 09:54:14 +0900

    사이드바 서브메뉴 개발 중

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@
 
     <div>
       <router-link to="/menu-test">menuTest</router-link> |
-      <router-link to="/list-test">listTest</router-link>
+      <router-link to="/list-test">listTest</router-link> |
+      <router-link to="/side-sub-menu-test">sideSubMenwTest</router-link>
     </div>
    <router-view/>
   </div>
