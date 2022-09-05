25d60fe70ef9c99e568f7362d05633e7f05b7c62
fms2014 <fms2014@naver.com>
2022-09-06 00:24:17 +0900

    헤더 메뉴 및 네비게이션 추가

--- src/layout/Header.vue
+++ src/layout/Header.vue
@@ -5,7 +5,11 @@
       <div class="prev"><span>&lt;-</span></div>
       <div class="next"><span>-&gt;</span></div>
       <div class="lastest"><span>최근</span></div>
-      <input type="search" placeholder="새 워크스페이스 검색" />
+      <div class="search-wrap">
+        <span class="search-workspace">검색</span>
+        <button class="fillter">fillter</button>
+        <button id="search-workspace-btn">새 워크스페이스 검색</button>
+      </div>
     </div>
     <div class="etc-panel">
       <div class="help-icon"><span>?</span></div>
@@ -33,14 +37,50 @@ header{
   .workspace-control-panel{
     display: flex;
     justify-content: center;
-    border: {
-      width: 1px;
-      color: black;
-      style: solid;
-    }
+    align-items: center;
     flex-grow: 8;
-    input{
-      width: 40%;
+    .prev,.next, .lastest{
+      margin: 12px;
+      cursor: pointer;
+    }
+    .search-wrap{
+      position: relative;
+      flex-basis: 40%;
+
+      button{
+        cursor: pointer;
+      }
+      .search-workspace{
+        position: absolute;
+        top: 50%;
+        right: 0px;
+        transform: translateY(-50%);
+      }
+      .fillter{
+        position: absolute;
+        top: 50%;
+        right: 40px;
+        transform: translateY(-50%);
+        cursor: pointer;
+      }
+      #search-workspace-btn {
+        width: 100%;
+        height: 32px;
+        margin-left: 12px;
+        text-align: left;
+        line-height: 32px;
+        padding: 0px 0px 0px 4px;
+        font: {
+          size: 14px;
+          weight: 700; 
+        }
+        border: {
+          width: 1px;
+          color: black;
+          style: solid;
+          radius: 8px;
+        }
+      }
     }
   }
   .system-menu-btn-wrap{
