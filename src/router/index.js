5f9c002ee2aef3161624f7dc100d4a329ecd7a24
fms2014 <fms2014@naver.com>
2022-09-10 13:25:02 +0900

    css에서 사용할 공통 변수 추가

--- src/router/index.js
+++ src/router/index.js
@@ -24,7 +24,7 @@ const routes = [
   {
     path: '/main-view',
     name: 'mainView',
-    component: MainView,
+    component: MainTest,
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
